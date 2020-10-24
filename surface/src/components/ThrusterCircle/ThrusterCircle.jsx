8f5edbc8dd4d55dcb9163ac0dfff16f7b6f55787
Ivan <ivan.kouz333@gmail.com>
2020-10-24 14:18:03 -0400

    Improved ThrusterCircle

--- surface/src/components/ThrusterCircle/ThrusterCircle.jsx
+++ surface/src/components/ThrusterCircle/ThrusterCircle.jsx
@@ -1,22 +1,20 @@
 import React from 'react';
-import {Col, Row, Container} from 'react-bootstrap';
+import {Container} from 'react-bootstrap';
 import './ThrusterCircle.css';
 
 export default class ThrusterCircle extends React.Component {
 	constructor(props) {
         super(props);
-        
-        this.state = {val: props.thrust};
 
         this.circleStyle = {
             backgroundImage: 'linear-gradient(91deg, transparent 50%, #A2ECFB 50%), linear-gradient(90deg, #A2ECFB 50%, transparent 50%)'
         };
 
         this.setValue = this.setValue.bind(this);
+    }
 
-        const poll = setInterval(() => {
-            this.setValue(this.props.thrust * 360);
-        }, 100);
+    componentDidUpdate(){
+        this.setValue(this.props.thrust * 360);
     }
     
     setValue(val) {
@@ -34,9 +32,9 @@ export default class ThrusterCircle extends React.Component {
 	render() {
 		return (
 			<Container>
-                <div id="activeBorder" class="active-border" style={this.circleStyle}>
-                    <div id="circle" class="circle">
-                        <span class="val 360" id="val">{Math.round(this.props.thrust * 100)}%</span>
+                <div className='active-border' style={this.circleStyle}>
+                    <div className='circle'>
+                        <span className='val 360'>{Math.round(this.props.thrust * 100)}%</span>
                     </div>
                 </div>
 			</Container>
