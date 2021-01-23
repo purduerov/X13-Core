095de8008978bfbc130155611f2bde7ee44fb131
jason <jason_bleasdell@hotmail.com>
2021-01-23 11:04:48 -0800

    adding servo control code to test

---
+++ ros/src/i2c/scripts/servo_talker.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+import rospy
+from std_msgs.msg import Float32
+
+def talker():
+    rospy.init_node('test_servo_talker_node', anonymous=True)
+    pub = rospy.Publisher('test_servor_anlges', Float32, queue_size=10)
+    rate = rospy.Rate(0.1)
+    i = 0
+    while not rospy.is_shutdown():
+        if i%2 == 0:
+            angle = 30.0
+            print(angle)
+            pub.publish(angle)
+        else:
+            angle = 120.0
+            print(angle)
+            pub.publish(angle)
+        i += 1
+
+        rate.sleep()
+
+if __name__ == '__main__':
+    try:
+        talker()
+    except rospy.ROSInterruptException:
+        pass
