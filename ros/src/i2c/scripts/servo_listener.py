1a01af88ef4db11bdb68e846fa3253653fecbf6d
Jonathan <jth3141@gmail.com>
2021-01-23 20:18:24 +0000

    okay tech it should work now

--- ros/src/i2c/scripts/servo_listener.py
+++ ros/src/i2c/scripts/servo_listener.py
@@ -7,19 +7,23 @@ import time
 from time import sleep #imports sleep (aka waiting or pause) into the program
 
 #Setting a Pin Mode
-GPIO.setmode(GPIO.BOARD) #Chose Board pin number scheme
+GPIO.setmode(GPIO.BCM) #Chose Board pin number scheme
 #Set up pin 11 for PWM
-GPIO.setup(11, GPIO.OUT) #sets up pin 11 to an output
-p = GPIO.PWM(11,50) #sets up pin 11 as a PWM pin(50 is the frequency)
+GPIO.setup(33, GPIO.OUT) #sets up pin 11 to an output
+p = GPIO.PWM(33,50) #sets up pin 11 as a PWM pin(50 is the frequency)
 p.start(0) #starts running PWM on the pin and sets it to 0. 0 is the middle, 
 duty_prev = 7.5 #in the middle
 
 def callback(requestedAngle):
     #change the angle to desired duty cycle
     duty = ((requestedAngle.data / 180) + 1 ) / 0.2 #((Angle you want / 180) + 1 for the period offset) / 2 for the total period))
+    global duty_prev
+
     if (duty != duty_prev):
         try:
-            duty == duty_prev
+            print(f"Duty: {duty}")
+            print(f"Duty Prev: {duty_prev}")
+            duty_prev = duty
             p.ChangeDutyCycle(duty)
             time.sleep(1)
         except:
