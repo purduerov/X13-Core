27df6c60ce342870ea4844315d72e6d6af32b843
Ivan Kouzmine <ivan.kouz333@gmail.com>
2021-04-10 16:45:22 -0400

    removed old jsx stuff

--- surface/src/index.js
+++
@@ -1,8 +0,0 @@
-/* eslint-env browser */
-import React from 'react';
-import { render } from 'react-dom';
-
-import MainWindow from './MainWindow.jsx';
-
-render(<MainWindow/>, document.getElementById('app'));
-
