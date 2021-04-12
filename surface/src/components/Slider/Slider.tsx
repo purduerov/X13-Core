a64f223ef76dae19a07fea604d3489055a4512b8
Ivan Kouzmine <ivan.kouz333@gmail.com>
2021-04-11 20:05:39 -0400

    added default props to slider hook

--- surface/src/components/Slider/Slider.tsx
+++ surface/src/components/Slider/Slider.tsx
@@ -8,21 +8,28 @@ interface Props{
     callback(val: number): void
 }
 
+const defaultProps: Props = {
+    vertical: false,
+    min: 0,
+    max: 100,
+    callback: (_) => {}
+}
 
-const Slider: React.FC<Props> = ({min, max, callback}) => {
+
+const Slider: React.FC<Props> = (props) => {
 
     const [value, setValue] = React.useState(0);
 
     return (
         <input 
             type='range' 
-            min={min} 
-            max={max}
+            min={props.min} 
+            max={props.max}
             value={value}
             className='slider'
             onChange={(e) => {
                 setValue(parseFloat(e.target.value));
-                callback(value);
+                props.callback(value);
             }}
         />
     )
