12bb1cedbaef15fef772393d3baaa3eef5376e5f
Ivan Kouzmine <ivan.kouz333@gmail.com>
2021-04-14 18:30:49 -0400

    ThrusterInfo moved to hooks

--- surface/src/components/Thrusters/ThrusterInfo/ThrusterInfo.tsx
+++ surface/src/components/Thrusters/ThrusterInfo/ThrusterInfo.tsx
@@ -5,35 +5,25 @@ import './ThrusterInfo.scss';
 
 const INIT_THRUST = 127;
 
-interface State{
-    thrust: Array<number>
+const ThrusterInfo: React.FC = () => {
+	const [thrust, setThrust] = React.useState<Array<number>>(Array(8).fill(INIT_THRUST));
+
+	ipcRenderer.on('thrusters', (e, data: Array<number>) => {
+		setThrust(data)
+	});
+
+	return (
+		<div className="thruster-container">
+			<ThrusterCircle className="thruster-left" thrust={thrust[4]} name='HFL'/>
+			<ThrusterCircle className="thruster-right" thrust={thrust[7]} name='HFR'/>
+			<ThrusterCircle className="thruster-right" thrust={thrust[0]} name='VFL'/>
+			<ThrusterCircle className="thruster-left" thrust={thrust[3]} name='VFR'/>
+			<ThrusterCircle className="thruster-right" thrust={thrust[1]} name='VBL'/>
+			<ThrusterCircle className="thruster-left" thrust={thrust[2]} name='VBR'/>
+			<ThrusterCircle className="thruster-left" thrust={thrust[5]} name='HBL'/>
+			<ThrusterCircle className="thruster-right" thrust={thrust[6]} name='HBR'/>        
+		</div>
+	)
 }
 
-export default class ThrusterInfo extends React.Component<{}, State> {
-	constructor(props: {}) {
-        super(props);
-
-        this.state = {thrust: [INIT_THRUST, INIT_THRUST, INIT_THRUST, INIT_THRUST, INIT_THRUST, INIT_THRUST, INIT_THRUST, INIT_THRUST]};
-    }
-
-	componentDidMount(){
-		ipcRenderer.on('thrusters', (e, data: Array<number>) => {
-			this.setState({thrust: data})
-		});
-	}
-
-	render() {
-		return (
-			<div className="thruster-container">
-                <ThrusterCircle className="thruster-left" thrust={this.state.thrust[4]} name='HFL'/>
-                <ThrusterCircle className="thruster-right" thrust={this.state.thrust[7]} name='HFR'/>
-				<ThrusterCircle className="thruster-right" thrust={this.state.thrust[0]} name='VFL'/>
-				<ThrusterCircle className="thruster-left" thrust={this.state.thrust[3]} name='VFR'/>
-				<ThrusterCircle className="thruster-right" thrust={this.state.thrust[1]} name='VBL'/>
-				<ThrusterCircle className="thruster-left" thrust={this.state.thrust[2]} name='VBR'/>
-				<ThrusterCircle className="thruster-left" thrust={this.state.thrust[5]} name='HBL'/>
-                <ThrusterCircle className="thruster-right" thrust={this.state.thrust[6]} name='HBR'/>        
-			</div>
-		);
-	}
-}
+export default ThrusterInfo;
