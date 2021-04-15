aaadb611b79252fc557bd914a74ab39a8c0290b4
Ivan Kouzmine <ivan.kouz333@gmail.com>
2021-04-14 21:12:45 -0400

    ThrusterCircle fix

--- surface/src/components/Thrusters/ThrusterCircle/ThrusterCircle.tsx
+++ surface/src/components/Thrusters/ThrusterCircle/ThrusterCircle.tsx
@@ -7,11 +7,12 @@ interface Props{
     name: string
 }
 
-const l180 = (val: number) => {
+const l180 = (val: number, original: number) => {
     return({
         backgroundImage:
             `linear-gradient(${val + 90}deg, transparent 50%, #A6A6A6 50%),` +
-            `linear-gradient(90deg, #A6A6A6 50%, transparent 50%)`
+            `linear-gradient(90deg, #A6A6A6 50%, transparent 50%)`,
+        backgroundColor: original < 127 ? '#FF4747' : '#39B4CC'
     })
 }
 
@@ -19,7 +20,8 @@ const g180 = (val: number, original: number) => {
     return({
         backgroundImage:
             `linear-gradient(${val - 90}deg, transparent 50%, ${original < 127 ? '#FF4747' : '#39B4CC'} 50%),` +
-            `linear-gradient(90deg, #A6A6A6 50%, transparent 50%)`
+            `linear-gradient(90deg, #A6A6A6 50%, transparent 50%)`,
+        backgroundColor: original < 127 ? '#FF4747' : '#39B4CC'
     })
 }
 
@@ -32,9 +34,9 @@ const ThrusterCircle: React.FC<Props> = (props) => {
     
     return(
         <div className={props.className}>
-            <div className='active-border' style={angle <= 180 ? l180(angle) : g180(angle, props.thrust)}>
+            <div className='active-border' style={angle <= 180 ? l180(angle, props.thrust) : g180(angle, props.thrust)}>
                 <div className='circle'>
-                    <span className='val 360'>{angle / 360 * 100}%</span>
+                    <span className='val 360'>{Math.round(angle / 360 * 100)}%</span>
                     <br/>
                     <span className='val 360'>{props.name}</span>
                 </div>
