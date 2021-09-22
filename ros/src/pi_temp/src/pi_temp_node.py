08b8c7db343a2f3a93dfe5905ec2c21c43f214af
Torin1914 <zach.t.neel@gmail.com>
2021-09-22 19:29:29 -0400

    created not which outputs the pi's temp?

---
+++ ros/src/pi_temp/src/pi_temp_node.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+# license removed for brevity
+import rospy
+from gpiozero import CPUTemperature
+from std_msgs.msg import String
+from std_msgs.msg import Float64
+
+def talker():
+    
+    cpu = CPUTemperature()
+    pub = rospy.Publisher(cpu.temperature, Float64, queue_size=10)
+    rospy.init_node('Pi_CPU_TEMP', anonymous=True)
+    rate = rospy.Rate(1) # 10hz
+    
+    while not rospy.is_shutdown():
+        hello_str = "hello world %s" % rospy.get_time()
+        rospy.loginfo(hello_str)
+        pub.publish(hello_str)
+        rate.sleep()
+   
+if __name__ == '__main__':
+    try:
+        talker()
+    except rospy.ROSInterruptException:
+        pass
