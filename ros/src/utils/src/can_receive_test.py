86a9145af3ad29ac287c69a2053ea7bb8fa38c65
jheidegg <jheidegg>
2021-10-04 12:37:30 -0500

    pooltest data

--- ros/src/utils/src/can_receive_test.py
+++ ros/src/utils/src/can_receive_test.py
@@ -8,6 +8,7 @@ import sys
 
 import can
 
+printToCSV = True
 lastEsc = [0] * 3
 
 
@@ -17,17 +18,24 @@ def bus_message_received(can_rx: "can.Message") -> None:
         lastEsc_ = lastEsc[can_id - 0x301]
         lastEsc[can_id - 0x301] = (lastEsc_ + 1) % 4
         data = list(can_rx.data)
-        print(f"ESC # {lastEsc_}")
-        print(f"Temperature {data[0]} C.")
-        print(f"Voltage {data[1] / 255 + 10} V.")
-        print(f"Current {(data[2] << 8 + data[3]) / 100} A.")
-        print(f"Energy {(data[4] << 8 + data[5])} mAHr.")
-        print(f"Speed {(data[6] << 8 + data[7]) * 100} erpm.\n")
+        if(printToCSV):
+            #removing garbage data with currents that don't make since
+            #TODO:figure out why data is garbage sometimes
+            
+            print(f"{lastEsc_},{data[0]},{data[1] / 255 + 10},{(data[2] << 8 + data[3]) / 100},{(data[4] << 8 + data[5])},{(data[6] << 8 + data[7]) * 100}")
+        else:
+            print(f"ESC # {lastEsc_}")
+            print(f"Temperature {data[0]} C.")
+            print(f"Voltage {data[1] / 255 + 10} V.")
+            print(f"Current {(data[2] << 8 + data[3]) / 100} A.")
+            print(f"Energy {(data[4] << 8 + data[5])} mAHr.")
+            print(f"Speed {(data[6] << 8 + data[7]) * 100} erpm.\n")
     else:
-        print(type(can_rx))
-        print(f"data: {list(can_rx.data)}")
-        print(f"id: {can_id:03X}")
-        print(f"message {can_rx}\n\n")
+        pass
+        # print(type(can_rx))
+        # print(f"data: {list(can_rx.data)}")
+        # print(f"id: {can_id:03X}")
+        # print(f"message {can_rx}\n\n")
 
 
 def main(args: list) -> None:
